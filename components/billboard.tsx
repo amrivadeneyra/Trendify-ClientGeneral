22f4bc21ddaf0746703056d3c316dd889a13cc78
Manuel Rivadeneyra <arivadeneyrai@autonoma.edu.pe>
2024-06-16 20:55:01 -0500

    Esilos para imagen de menu principal

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -6,15 +6,16 @@ interface BillboardProps {
 
 export const Billboard: React.FC<BillboardProps> = ({ data }) => {
   return (
-    <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
+    <div className="w-full h-screen overflow-hidden">
       <div
-        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
+        className="relative w-full h-full bg-cover bg-center"
         style={{
           backgroundImage: `url(${data?.imageUrl})`,
         }}
       >
+        <div className="absolute inset-0 bg-black opacity-40"></div>
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
-          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
+          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs z-10 text-white">
             {data?.label}
           </div>
         </div>
