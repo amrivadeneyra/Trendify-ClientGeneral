fd193d63f120b532b82f09ad7356b127740f4d59
Manuel Rivadeneyra <arivadeneyrai@autonoma.edu.pe>
2024-06-16 20:55:44 -0500

    Se agrega boton de menu en vista movil

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,13 +10,29 @@ export const Navbar = async () => {
   return (
     <div className="border-b">
       <Container>
-        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+        <div className="px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">STORE</p>
+            <p className="font-bold text-2xl">TRENDIFY</p>
           </Link>
-
-          <MainNav data={categories} />
-
+          <div className="hidden md:block">
+            <MainNav data={categories} />
+          </div>
+          <div className="md:hidden">
+            <button
+              className="p-2 rounded-md text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+            </button>
+          </div>
           <NavbarActions />
         </div>
       </Container>
