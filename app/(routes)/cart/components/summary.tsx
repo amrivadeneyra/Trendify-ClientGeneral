774834122cc393016f1be06683db6087a7e80b42
Manuel Rivadeneyra <arivadeneyrai@autonoma.edu.pe>
2024-07-09 00:05:35 -0500

    diseños para el pdf

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -8,6 +8,7 @@ import { useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
 import toast from 'react-hot-toast'
 import jsPDF from 'jspdf'
+import 'jspdf-autotable';
 
 export const Summary = () => {
   const searchParams = useSearchParams()
@@ -17,7 +18,7 @@ export const Summary = () => {
   useEffect(() => {
     if (searchParams.get('success')) {
       generatePDF()
-      toast.success('Your order has been placedtext!')
+      toast.success('Your order has been placed!')
       removeAll()
     }
 
@@ -42,15 +43,51 @@ export const Summary = () => {
   }
 
   const generatePDF = () => {
-    const doc = new jsPDF()
+    const doc = new jsPDF();
 
-    doc.text('Order Summary', 10, 10)
+    const styles = {
+      title: { fontSize: 18, font: 'helvetica', fontStyle: 'bold', color: 'black' },
+      subtitle: { fontSize: 14, font: 'helvetica', fontStyle: 'normal', color: 'gray' },
+      details: { fontSize: 12, font: 'helvetica', fontStyle: 'normal', color: 'black' },
+      price: { fontSize: 14, font: 'helvetica', fontStyle: 'bold', color: 'black' },
+      separator: { height: 1, backgroundColor: 'gray', margin: '10px 0' },
+    };
+
+    let startY = 20;
+
     items.forEach((item, index) => {
-      doc.text(`${index + 1}. ${item.name} - ${item.price}`, 10, 20 + index * 10)
-    })
+      doc.setFont(styles.title.font, styles.title.fontStyle);
+      doc.setFontSize(styles.title.fontSize);
+      doc.setTextColor(styles.title.color);
+      doc.text(item.name, 15, startY + 10);
 
-    doc.save('order.pdf')
-  }
+      doc.setFont(styles.subtitle.font, styles.subtitle.fontStyle);
+      doc.setFontSize(styles.subtitle.fontSize);
+      doc.setTextColor(styles.subtitle.color);
+      doc.text(`Category: ${item.category}`, 15, startY + 20);
+
+      doc.setFont(styles.details.font, styles.details.fontStyle);
+      doc.setFontSize(styles.details.fontSize);
+      doc.setTextColor(styles.details.color);
+      doc.text(`Size: ${item.size}`, 15, startY + 30);
+      doc.text(`Color: ${item.color}`, 80, startY + 30);
+
+      doc.setFont(styles.price.font, styles.price.fontStyle);
+      doc.setFontSize(styles.price.fontSize);
+      doc.setTextColor(styles.price.color);
+      doc.text(`Price: $${item.price}`, 15, startY + 40);
+
+      doc.setDrawColor(styles.separator.backgroundColor);
+      doc.setLineWidth(styles.separator.height);
+      doc.line(15, startY + 45, 190, startY + 45);
+
+      startY += 60; // Ajusta según la altura total de la sección
+    });
+
+    doc.save('order.pdf');
+
+  };
+
 
   return (
     <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
